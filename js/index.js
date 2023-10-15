617116cb786bda5d68681aca9f1cdd936f53704f
Carislost <135508493+Carislost@users.noreply.github.com>
2023-10-15 16:42:27 -0600

    Validacio al CRUD Jugador
    se crearon validaciones para nombre,apellido,edad y email.

--- js/index.js
+++ js/index.js
@@ -1,36 +1,68 @@
 import { SaveJugador, getJugador } from "./main.js";
 
+function ValidacionJugador() {
+  const nombreJug = document.getElementById('jugadorNombre').value;
+  const apellidoJug = document.getElementById('jugadorApellido').value;
+  const edadJug = document.getElementById('jugadorEdad').value;
+  const emailJug = document.getElementById('jugadorEmail').value;
+
+  if (!isNaN(nombreJug) || nombreJug.trim() === "") {
+    alert("Debe ingresar un nombre para el jugador");
+    return false;
+  }
+
+  if (!isNaN(apellidoJug) || apellidoJug.trim() === "") {
+    alert("Debe ingresar un apellido para el jugador");
+    return false;
+  }
+
+  if(isNaN(edadJug) || edadJug.trim() === ""){
+    alert("Debe ingresar una edad para el jugador");
+    return false;
+  }
+
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+  if(!emailRegex.test(emailJug)) {
+    alert("Debe ingresar un email valido");
+    return false;
+  }
+
+  return true;
+}
 
 window.addEventListener("DOMContentLoaded", async () => {
-    const jugadorList = document.getElementById("tablaJugador");
-    
-    const querySnapshot = await getJugador();
-    querySnapshot.forEach((doc) => {
-      let player = doc.data();
-      let row = document.createElement('tr');
-      row.innerHTML = `
+  const jugadorList = document.getElementById("tablaJugador");
+
+  const querySnapshot = await getJugador();
+  querySnapshot.forEach((doc) => {
+    let player = doc.data();
+    let row = document.createElement('tr');
+    row.innerHTML = `
         <td>${player.nombre}</td>
         <td>${player.apellido}</td>
         <td>${player.edad}</td>
         <td>${player.email}</td>
       `;
-      jugadorList.appendChild(row);
-    });
+    jugadorList.appendChild(row);
   });
+});
 
 const JugadorForm = document.getElementById("Jugador-Form");
 
 JugadorForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("Enviado");
+  if (ValidacionJugador()) {
+    console.log("Enviado");
 
-  const nombreJug = document.getElementById("jugadorNombre").value;
-  const apellidoJug = document.getElementById("jugadorApellido").value;
-  const edadJug = document.getElementById("jugadorEdad").value;
-  const emailJug = document.getElementById("jugadorEmail").value;
+    const nombreJug = document.getElementById("jugadorNombre").value;
+    const apellidoJug = document.getElementById("jugadorApellido").value;
+    const edadJug = document.getElementById("jugadorEdad").value;
+    const emailJug = document.getElementById("jugadorEmail").value;
 
-  SaveJugador(nombreJug, apellidoJug, edadJug, emailJug);
+    SaveJugador(nombreJug, apellidoJug, edadJug, emailJug);
 
-  JugadorForm.reset();
-  window.onload();
+    JugadorForm.reset();
+    window.onload();
+  }
 });
