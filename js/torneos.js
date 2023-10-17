98984d5c73f06140d2cfbdc6cb4934c975e115b4
Jonathan0497 <jonathanmendoza2001.jm@gmail.com>
2023-10-16 21:22:07 -0600

    CRUD Torneos
    Se creo la interfaz de torneos

---
+++ js/torneos.js
@@ -0,0 +1,12 @@
+import {
+    SaveJugador,
+    getJugador,
+    deleteJugador,
+    updateJugador,
+    onGetJugador,
+    db,
+    collection,
+    query,
+    where,
+    getDocs
+  } from "./main.js";
