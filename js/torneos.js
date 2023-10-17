fc6c90290b23d7b711935fb81210da8e2db4415b
Carislost <135508493+Carislost@users.noreply.github.com>
2023-10-17 01:39:07 -0600

    Validacion CRUD torneo
    se validaron todos los campos que se necesitaban.

--- js/torneos.js
+++ js/torneos.js
@@ -11,6 +11,23 @@ import {
     getDocs,
 } from "./main.js";
 
+function ValidacionTorneo() {
+    const TorneoNombre = document.getElementById('TorneoNombre').value;
+    const DireccionTorneo = document.getElementById('DireccionTorneo').value;
+
+    if (!isNaN(TorneoNombre) || TorneoNombre.trim() === "") {
+        alert("Debe ingresar un nombre para el torneo");
+        return false;
+    }
+
+    if (!isNaN(DireccionTorneo) || DireccionTorneo.trim() === "") {
+        alert("Debe ingresar una dirección para el torneo");
+        return false;
+    }
+
+    return true;
+}
+
 const torneoList = document.getElementById("tablaTorneo");
 window.addEventListener("DOMContentLoaded", async () => {
     onGetTorneo((querySnapshot) => {
@@ -60,24 +77,36 @@ window.addEventListener("DOMContentLoaded", async () => {
 const TorneoForm = document.getElementById("Torneo-Form");
 TorneoForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const id_torneo = document.getElementById("id_torneo").value;
-    const TorneoNombre = document.getElementById("TorneoNombre").value;
-    const CantidadJugadoresTorneos = document.getElementById("CantidadJugadoresTorneos").value;
-    const estadoTorneo = document.getElementById("estadoTorneo").value;
-    const FechaInicioTorneo = document.getElementById("FechaInicioTorneo").value;
-    const DireccionTorneo = document.getElementById("DireccionTorneo").value;
+    const selectedValue = document.getElementById('CantidadJugadoresTorneos').value;
+    const fechaInicio = document.getElementById('FechaInicioTorneo').value;
+    if (selectedValue !== "4" && selectedValue !== "8" && selectedValue !== "12") {
+        alert('Por favor, seleccione 4, 8 o 12 jugadores');
+        return;
+    }
+    if (!fechaInicio) {
+        alert('Por favor, ingrese una fecha de inicio');
+        return;
+    }
 
-    if (id_torneo === '') {
-        //SaveTorneos(TorneoNombre, CantidadJugadoresTorneos, estadoTorneo, FechaInicioTorneo, DireccionTorneo)
-        console.log(TorneoNombre, CantidadJugadoresTorneos, estadoTorneo, FechaInicioTorneo, DireccionTorneo)
-    } else {
-        updateTorneo(id_torneo, {
-            nombre: TorneoNombre,
-            cantidadJugadores: CantidadJugadoresTorneos,
-            estado: estadoTorneo,
-            fechaInicio: FechaInicioTorneo,
-            direccion: DireccionTorneo
-        })
+    if (ValidacionTorneo()) {
+        const id_torneo = document.getElementById("id_torneo").value;
+        const TorneoNombre = document.getElementById("TorneoNombre").value;
+        const CantidadJugadoresTorneos = document.getElementById("CantidadJugadoresTorneos").value;
+        const estadoTorneo = document.getElementById("estadoTorneo").value;
+        const FechaInicioTorneo = document.getElementById("FechaInicioTorneo").value;
+        const DireccionTorneo = document.getElementById("DireccionTorneo").value;
+
+        if (id_torneo === '') {
+            SaveTorneos(TorneoNombre, CantidadJugadoresTorneos, estadoTorneo, FechaInicioTorneo, DireccionTorneo)
+            console.log(TorneoNombre, CantidadJugadoresTorneos, estadoTorneo, FechaInicioTorneo, DireccionTorneo)
+        } else {
+            updateTorneo(id_torneo, {
+                nombre: TorneoNombre,
+                cantidadJugadores: CantidadJugadoresTorneos,
+                estado: estadoTorneo,
+                fechaInicio: FechaInicioTorneo,
+                direccion: DireccionTorneo
+            })
+        }
     }
 })
-
