e65eb29adb7896a0df32b75a4203a41161637a00
Carislost <135508493+Carislost@users.noreply.github.com>
2023-11-20 21:25:39 -0600

    Reglas
    se agrego Reglas a todos los menus y las reglas de ping pong.

---
+++ js/Regla.js
@@ -0,0 +1,115 @@
+const secciones = {
+    equipamiento: `
+      <div class="card">
+        <div class="card-header">
+          Equipamiento y Área de Juego
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Equipamiento y Área de Juego del Ping Pong:</h5>
+          <p class="card-text">
+          <strong>Mesa:</strong> Debe tener 2.74 metros de largo, 1.525 metros de ancho y 76 centímetros de altura. La superficie debe ser de color oscuro, mate y uniforme, con líneas blancas que delimitan los bordes y la división central.
+          </p>
+          <p class="card-text"><strong style="color: rgb(55, 55, 62);">Red:</strong> La red divide la mesa en dos campos iguales y tiene una altura de 15.25 centímetros sobre la superficie de juego.</p>
+          <p class="card-text"><strong style="color: rgb(55, 55, 62);">Pelotas:</strong> Son esféricas, con un diámetro de 40mm y un peso de 2.7 gramos, y pueden ser de color blanco o naranja con una superficie mate.</p>
+          <p class="card-text"><strong style="color: rgb(55, 55, 62);">Raquetas:</strong> Compuestas por una hoja de madera plana y rígida, recubiertas por goma en una o ambas caras, la cual puede tener diferentes texturas.</p>
+          <p class="card-text"><strong style="color: rgb(55, 55, 62);">Área de Juego:</strong> El espacio debe ser suficientemente amplio para permitir el libre movimiento de los jugadores y estar delimitado para evitar distracciones. La iluminación debe ser uniforme y sin reflejos.</p>
+        </div>
+      </div>
+    `,
+    servicio: `
+      <div class="card">
+        <div class="card-header">
+          Servicio y Recepción
+        </div>
+        <div class="card-body">
+        <h5 class="card-title">Servicio</h5>
+        <p class="card-text"><strong style="color: rgb(55, 55, 62);">Posición Inicial:</strong>El servidor comienza con la pelota descansando sobre la palma abierta y estacionaria de la mano libre.</p>
+    <p class="card-text"><strong style="color: rgb(55, 55, 62);">Lanzamiento de la Pelota:</strong>La pelota debe lanzarse verticalmente hacia arriba, al menos 16 cm, sin impartirle spin.</p>
+    <p class="card-text"><strong style="color: rgb(55, 55, 62);">Golpe de la Pelota:</strong>Después de lanzarla, el servidor debe golpear la pelota de manera que primero toque su propio campo y luego, al pasar la red, toque el campo del receptor.</p>
+    <p class="card-text"><strong style="color: rgb(55, 55, 62);">Regla Específica para Dobles:</strong>En juegos de dobles, la pelota debe tocar sucesivamente el lado derecho de ambos campos, tanto del servidor como del receptor.</p>
+    <br>
+    <h5 class="card-title">Recepción</h5>
+    <p class="card-text"><strong style="color: rgb(55, 55, 62);">Golpe de Retorno:</strong>El receptor debe golpear la pelota de tal manera que, tras pasar sobre o alrededor de la red, toque el campo del servidor.</p></p>
+    <p class="card-text"><strong style="color: rgb(55, 55, 62);">Restricción de Golpe:</strong>El receptor no puede golpear la pelota hasta que esta haya tocado su campo.</p>
+    <p class="card-text"><strong style="color: rgb(55, 55, 62);"></strong></p>
+        </div>
+      </div>
+    `,
+    puntuacion: `
+      <div class="card">
+        <div class="card-header">
+          Puntuación
+        </div>
+        <div class="card-body">
+        <h5 class="card-title">Reglas de Puntuación</h5>
+        <p class="card-text">
+          En el tenis de mesa, la puntuación es un aspecto crucial que define el flujo del juego. Un jugador o equipo gana un punto cuando el oponente comete un error o no logra devolver la pelota de acuerdo con las reglas. El juego se juega normalmente al mejor de cinco o siete juegos. Cada juego se gana por el jugador o equipo que primero alcance los 11 puntos, con una diferencia mínima de dos puntos. Esto significa que si el juego llega a un empate de 10-10, se continúa jugando hasta que uno de los jugadores o equipos logre una ventaja de dos puntos.
+          <br><br>
+          El servicio rota cada dos puntos hasta que ambos jugadores o equipos alcancen los 10 puntos. Una vez en esta etapa de 'deuce', el servicio rota después de cada punto. Esta rotación continua y rápida del servicio exige que los jugadores estén siempre atentos y preparados.
+          <br><br>
+          En caso de un error en el servicio o una falta durante el juego, el punto se otorga al oponente. Los errores comunes incluyen no golpear la pelota sobre la red, golpear la pelota fuera del área de juego del oponente, o no realizar un servicio válido. Estar al tanto de estas reglas de puntuación es esencial para jugar y seguir un partido de tenis de mesa de manera efectiva.
+        </p>
+        
+        </div>
+      </div>
+    `,
+    reglasJuego: `
+      <div class="card">
+        <div class="card-header">
+          Reglas de Juego
+        </div>
+        <div class="card-body">
+        <h5 class="card-title">Normativas Durante el Juego</h5>
+        <p class="card-text">
+          Las reglas durante un juego de tenis de mesa son fundamentales para asegurar un juego limpio y justo. Un punto comienza con el servicio y continúa hasta que se comete un error o una falta. Los errores incluyen no devolver la pelota, que la pelota toque cualquier objeto fuera de la mesa, o que la pelota golpee dos veces en un lado antes de cruzar la red. Una falta se comete si el jugador toca la mesa con su mano libre durante un punto, mueve la mesa, o obstruye la pelota con su cuerpo o raqueta sin haber sido golpeada primero por el oponente.
+          <br><br>
+          El jugador que sirve debe hacerlo de manera que la pelota sea visible para el oponente en todo momento. No se permite ocultar la pelota con ninguna parte del cuerpo o la vestimenta durante el servicio. En dobles, los jugadores deben alternarse en golpear la pelota. Un punto se concede al oponente si un jugador golpea la pelota fuera del orden correcto.
+          <br><br>
+          Estas reglas se aplican en todos los niveles de juego y son esenciales para mantener el espíritu competitivo y la integridad del juego.
+        </p>
+        
+        </div>
+      </div>
+    `,
+    dobles: `
+      <div class="card">
+        <div class="card-header">
+          Juego de Dobles
+        </div>
+        <div class="card-body">
+        <h5 class="card-title">Modalidad de Dobles</h5>
+        <p class="card-text">
+          En la modalidad de dobles del tenis de mesa, las reglas se ajustan ligeramente para acomodar a dos jugadores en cada lado de la mesa. Los jugadores deben alternar golpes durante un punto. Después de que el servicio ha sido devuelto, cualquier jugador del equipo puede golpear la pelota. Importante es que los jugadores deben seguir el orden establecido al inicio del juego y no pueden golpear la pelota dos veces consecutivas.
+          <br><br>
+          El servicio en dobles debe ejecutarse de tal manera que la pelota toque primero el lado derecho del servidor y luego el lado derecho del receptor. Al igual que en singles, el servicio rota cada dos puntos, y cada jugador del equipo debe servir en su turno antes de que el servicio pase al equipo contrario.
+        </p>
+        
+        </div>
+      </div>
+    `,
+    conducta: `
+      <div class="card">
+        <div class="card-header">
+          Conducta y Fair Play
+        </div>
+        <div class="card-body">
+        <h5 class="card-title">Comportamiento y Juego Limpio</h5>
+        <p class="card-text">
+          El tenis de mesa, como cualquier deporte, exige un alto nivel de ética deportiva y respeto por las reglas del juego y los oponentes. Los jugadores deben mantener una actitud de juego limpio y respeto mutuo en todo momento. Esto incluye evitar comportamientos antideportivos, como lanzar la raqueta, usar lenguaje ofensivo o disputar las decisiones de los árbitros de manera irrespetuosa.
+          <br><br>
+          Se espera que los jugadores respeten el equipamiento y las instalaciones, y se comporten de manera profesional dentro y fuera de la mesa. La conducta antideportiva puede resultar en advertencias, penalizaciones de puntos o incluso en la descalificación del juego o del torneo.
+        
+        </p>
+        
+        </div>
+      </div>
+    `,
+};
+
+function mostrarSeccion(seccion) {
+    const contenido = secciones[seccion] || '<div class="alert alert-danger" role="alert">Sección no encontrada.</div>';
+    document.getElementById('contenido-reglas').innerHTML = contenido;
+}
+
+// Mostrar la primera sección por defecto al cargar la página
+document.addEventListener('DOMContentLoaded', () => mostrarSeccion('equipamiento'));
